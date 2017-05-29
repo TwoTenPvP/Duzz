555b5ff3cdc015c0aad7696d217dd7d43060cee8
TwoTen <twoten@twoten.io>
2017-05-29 18:23:17 +0200

    Added MUTEX to only allow one instance of application

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,12 +13,23 @@ namespace Client
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        /// 
+        static Mutex mutex = new Mutex(true, "DUZZ_MUTX_586453c8-147e-4947-9ae6-1b55f93f883b");
         [STAThread]
         static void Main()
         {
-            //TODO
-            NetworkManager.Connect();
-            NetworkManager.StartReconnect();
+            if (mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                //ENTRY
+                NetworkManager.Connect();
+                NetworkManager.StartReconnect();
+
+                mutex.ReleaseMutex();
+            }
+            else
+            {
+                //Mulitple instances!
+            }
         }
     }
 }
